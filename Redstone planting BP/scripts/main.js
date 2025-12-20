9a4c2f89e5933397147ede6e8636f30d744ac483
Lucas Miller <lucas139miller@gmail.com>
2025-12-20 18:42:39 -0300

    Plant BBlock Updated! - Now it really works for redstone

--- Redstone planting BP/scripts/main.js
+++ Redstone planting BP/scripts/main.js
@@ -1,13 +1,44 @@
-import { world } from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 
+let WatchedPosition = [];//position of block to be watched
+
+
+
+//BLOCK PLACED
 world.afterEvents.playerPlaceBlock.subscribe(ev => {
     const { block, player } = ev;
-
-    // Alert for the block connected
     if (block.typeId === "miller:plant_block") {
+        WatchedPosition.push(block.location);
+        player.sendMessage("Novo bloco em observação");
+        player.sendMessage(`Bloco número ${WatchedPosition.length}`);
+    }
+});
+
+//BLOCK REMOVED
+world.afterEvents.playerBreakBlock.subscribe(ev => {
+    const { block, player } = ev;
+    player.sendMessage("Bloco quebrado");
+    //search for index of block in same position that we want
+    const index = WatchedPosition.findIndex(loc => 
+        loc.x === block.location.x &&
+        loc.y === block.location.y &&
+        loc.z === block.location.z);
+    if(index !== -1){
+        //removing block from the list
+        WatchedPosition.splice(index,1);
+        player.sendMessage("Bloco removido da lista de observação");
+    }
+});
+
+//LOOP FOR PLANT BLOCK FUNCTION
+system.runInterval(()=>{
+    const dimension = world.getDimension("overworld");
+    for(const loc of WatchedPosition){ 
+       
+        const block = dimension.getBlock(loc);//block watched
+        if (!block) continue;
 
         const belowBlock = block.below();
-
         if( belowBlock.typeId === "minecraft:redstone_torch" || 
             belowBlock.typeId === "minecraft:redstone_block" ||
             (belowBlock.typeId === "minecraft:redstone_wire" && 
@@ -15,37 +46,24 @@ world.afterEvents.playerPlaceBlock.subscribe(ev => {
             )
         ){
             //POWERED BY REDSTONE
-            player.sendMessage("Conectado à redstone!");
+            world.sendMessage("Conectado à redstone!");
 
             const aboveBlock = block.above();
             const upperBlock = block.above(2);
 
             //CHECKING ABOVE BLOCKS
             if(aboveBlock.typeId === "minecraft:farmland" && upperBlock.typeId === "minecraft:air"){
-                player.sendMessage("Possível de plantar");
+                world.sendMessage("Possível de plantar");
 
                 const local = block.location;
                 //LOCAL TO BE PLANTED
                 const selectedPos = {x: local.x, y: local.y+2, z: local.z};
                 //PLANTING USING COMMAND
-                player.runCommand(`setblock ${selectedPos.x} ${selectedPos.y} ${selectedPos.z} wheat ["growth"=0] replace`);
+                dimension.runCommand(`setblock ${selectedPos.x} ${selectedPos.y} ${selectedPos.z} wheat ["growth"=0] replace`);
                 //WARNING
-                player.sendMessage("Bloco de trigo colocado!");
+                world.sendMessage("Bloco de trigo colocado!");
             }
 
         }
     }
-});
-
-//redstone_wire
-//redstone_torch - não precisa de estado ou variação
-//redstone_block
-
-/*//PLACE THE WANTED BLOCK
-selectedBlock.setType("minecraft:wheat");*/
-
-//const selectedBlock = world.getDimension("overworld").getBlock(selectedPos);
-//const dimension = world.getDimension("overworld");
-                //dimension.runCommand(`setblock ${selectedPos.x} ${selectedPos.y} ${selectedPos.z} minecraft:wheat 0`);
-                //selectedBlock.setPermutation(wheatSmall);
-                
+}, 10);
