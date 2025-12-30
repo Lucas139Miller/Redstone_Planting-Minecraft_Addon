ebf1fda5e0ce1a9d8d67eaaace04c699659e1f9c
Lucas Miller <lucas139miller@gmail.com>
2025-12-29 23:37:58 -0300

    Script bug updated! Now It can works after exiting and opening the world again

--- Redstone planting BP/scripts/main.js
+++ Redstone planting BP/scripts/main.js
@@ -1,41 +1,19 @@
 import { world, system } from "@minecraft/server";
 
 let WatchedPosition = [];//position of block to be watched
-
-
-//GAME MEMORY *************************************************************************
-//DEFINING SAVE AND LOAD FOR PLANT_BLOCK
-
-system.system.runTimeout(()=>{
-    world.sendMessage("INICIOU!");
-    let watchedStr = world.getDynamicProperty("watched_position");//string for confference
-
-    if(watchedStr != undefined){
-        WatchedPosition = JSON.parse(watchedStr);//load in array-object[JSON] form
-        world.sendMessage("Valores Carregados?");
-    }else{
-        world.sendMessage("Sem dados nó save!");//nothing happens!
-    }
-},1);
+let ver = 0;//for saving
 
 //SAVE PROCESS
-
 function saveWatched(){
-    let watchedStr = JSON.stringify(WatchedPosition);//converts to string value
+    let watchedStr = JSON.stringify(WatchedPosition);//conve1rts to string value
     world.setDynamicProperty("watched_position",watchedStr);//save in world file (string format)
-    world.sendMessage("Modificalção salva");
 }
-
-//*************************************************************************************
 
 //BLOCK PLACED
 world.afterEvents.playerPlaceBlock.subscribe(ev => {
     const { block, player } = ev;
     if (block.typeId === "miller:plant_block") {
         WatchedPosition.push(block.location);
-        player.sendMessage("Novo bloco em observação");
-        player.sendMessage(`Bloco número ${WatchedPosition.length}`);
-
         saveWatched();//save
     }
 });
@@ -43,7 +21,6 @@ world.afterEvents.playerPlaceBlock.subscribe(ev => {
 //BLOCK REMOVED
 world.afterEvents.playerBreakBlock.subscribe(ev => {
     const { block, player } = ev;
-    //player.sendMessage("Bloco quebrado");
     //search for index of block in same position that we want
     const index = WatchedPosition.findIndex(loc => 
         loc.x === block.location.x &&
@@ -52,13 +29,30 @@ world.afterEvents.playerBreakBlock.subscribe(ev => {
     if(index !== -1){
         //removing block from the list
         WatchedPosition.splice(index,1);
-        player.sendMessage("Bloco removido da lista de observação");
         saveWatched()//save
     }
 });
 
 //LOOP FOR PLANT BLOCK FUNCTION
 system.runInterval(()=>{
+
+        // Wait for 1 seccond
+    let watchedStr;
+        if (!ver) {
+          try{
+            watchedStr = world.getDynamicProperty("watched_position");
+            //transformar e salvar
+            WatchedPosition = JSON.parse(watchedStr);//load in array-object[JSON] form
+          }catch(e){
+            watchedStr = JSON.stringify(WatchedPosition);//conve1rts to string value
+            saveWatched();
+            console.warn("Error: " + e);
+          }
+        ver = 1;
+        }
+
+
+
     const dimension = world.getDimension("overworld");
     for(const loc of WatchedPosition){ 
        
@@ -99,7 +93,6 @@ system.runInterval(()=>{
                 block.setPermutation(
                     block.permutation.withState("miller:activated", true)
                 )
-
                 //world.sendMessage("Bloco de trigo colocado!");
             }
 
